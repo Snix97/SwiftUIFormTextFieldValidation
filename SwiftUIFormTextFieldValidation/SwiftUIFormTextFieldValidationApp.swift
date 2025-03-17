f8d4cd00f59ed697e38e1c033b91e9c40baf5210
Claire Roughan <ClaireRoughan.Dev@gmail.com>
2025-03-17 20:35:42 +0000

    Project set up

---
+++ SwiftUIFormTextFieldValidation/SwiftUIFormTextFieldValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIFormTextFieldValidationApp.swift
+//  SwiftUIFormTextFieldValidation
+//
+//  Created by Claire Roughan on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIFormTextFieldValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
